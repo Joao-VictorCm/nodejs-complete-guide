cbec181c6b0688bb16c3d84495ffa8af4db85a4e
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-06-04 22:12:41 -0300

    To tentando

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const http = require("http");
-
 let port = 3000;
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write(
+      <html>
+        res.write(
+        <head>
+          <title>Enter message</title>
+        </head>
+        ) res.write(
+        <body>
+          <form action="/message" method="POST">
+            <input type="text" name="message"></input>
+          </form>
+        </body>
+        ) res.write
+      </html>
+    );
+
+    return res.end();
+  }
+
   console.log(`servidor rodando na porta ${port}, ${req}`);
 });
 
