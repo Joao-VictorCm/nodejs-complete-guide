68424a975113434f57f4cfeea32adb69d1687807
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-06-09 21:06:29 -0300

    Inicando dnv

--- app.js
+++ app.js
@@ -2,28 +2,11 @@ const http = require("http");
 let port = 3000;
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    res.write(
-      <html>
-        res.write(
-        <head>
-          <title>Enter message</title>
-        </head>
-        ) res.write(
-        <body>
-          <form action="/message" method="POST">
-            <input type="text" name="message"></input>
-          </form>
-        </body>
-        ) res.write
-      </html>
-    );
-
-    return res.end();
-  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Page 01</title></head>");
+  res.write("<body><h1>ola</h1></body>");
+  res.write("</html>");
 
   console.log(`servidor rodando na porta ${port}, ${req}`);
 });
