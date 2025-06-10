882b93a794714ce3dcc7a00f1e97474d547b1deb
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-06-09 21:57:29 -0300

    Salvando a message do cliente

--- app.js
+++ app.js
@@ -16,7 +16,18 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
+    const body = [];
+    req.on("data", (chunk) => {
+      // "req.on" Para ouvir eventos, nesse caso a messagem digitada pelo usuario
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
